9eb2074d16a8a4b3d69052f3b30e7ef99fd1861a
sameerkali <s.faridi007@gmail.com>
2024-12-18 21:47:30 +0530

    fix the card input and button files (maybee)

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
-import { Button } from "./ui/button";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
